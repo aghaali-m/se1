8e060662ff31fb858664ef11e7b52eae7c315367
aghaali-m <z88727520@gmail.com>
2026-09-19 22:29:27 +0430

    random temp

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,11 @@
+import random
+
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
 def show_temp_view(request):
-    return HttpResponse("همدان 27 درجه سانتی گراد")
+    temp=random.randint(10,35)
+    context={'city':'همدان','temp':temp}
+    return render(request,'show_temp.html',context)
