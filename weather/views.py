d7741927e443e60d3ec04a91b250de83dec8a07a
aghaali-m <z88727520@gmail.com>
2026-09-19 23:10:11 +0430

    real temp

--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,19 @@
 import random
 
+from django.contrib.sites import requests
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
 def show_temp_view(request):
-    temp=random.randint(10,35)
+    lat,lon=37.7981,48.5140
+    url=f"https://api.openـmeteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+    try:
+        data=requests.get(url).json()
+        temp=data['current weather']['tempreture']
+    except:
+        temp=None
+
     context={'city':'همدان','temp':temp}
     return render(request,'show_temp.html',context)
