f469843c41990804439005bc61227bddaf50d870
aghaali-m <z88727520@gmail.com>
2026-09-23 13:47:24 +0330

    se2

--- weather/views.py
+++ weather/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse
 
 def show_temp_view(request):
     lat,lon=37.7981, 48.5146
-    url=f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+    url= f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
     try:
         data=requests.get(url).json()
         temp=data["current_weather"]["temperature"]
